77b184a2b8d540de0d6861bac37031c89580a962
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-01-08 20:46:18 -0600

    Mathematical Functions General Project
    This is my first script for the Bash Shell on Ubuntu GNU/Linux and its purpose is to use the 'pactl' utility to create output channels that combine both audio playback and recording, so that all applications direct their combined microphone output to the speakers, and there is an option to reset the configuration to factory defaults by the operating system.

--- Streaming.sh
+++ Streaming.sh
@@ -3,138 +3,148 @@
 # Bash program to activate or deactivate the mixing of audio and video
 # playback and recording channels.
 # Created by: Gustavo Islas Gálvez.
-# Generation Date: Thursday, January 08, 2026. (2nd Version).
+# Generation Date: Thursday, January 08, 2026. (3rd Version).
 
-# Function to deactivate and clean
+# Function to deactivate and clean the Stream mode
 disable_stream()
 {
-	echo "Restoring original settings..."
+	$(which echo) "Restoring original settings..."
 
 	# Download loopback and null-sink modules by name if they exist
-	pactl unload-module module-loopback
-	pactl unload-module module-null-sink
-	pactl unload-module module-remap-sink
+	$(which pactl) unload-module module-loopback
+	$(which pactl) unload-module module-null-sink
+	$(which pactl) unload-module module-remap-sink
 
 	# Set the sink (output) as default
-	pactl set-default-sink "$(pactl get-default-sink)"
+	$(which pactl) set-default-sink "$(pactl get-default-sink)"
 
-	echo ""
-	echo "Resetting all applications to the default sink... ($(pactl get-default-sink))..."
+	$(which echo) ""
+	$(which echo) "Resetting all applications to the default sink... ($(pactl get-default-sink))..."
 
 	# Move all applications back to the default sink
-	pactl list short sink-inputs |
+	$(which pactl) list short sink-inputs |
 	while read -r line; do
 		# We obtain the application index
-		echo "$line" | mawk '{print $1}'
-		app_index=$(echo "$line" | mawk '{print $1}')
-		pactl move-sink-input "$app_index" "$(pactl get-default-sink)"
+		$(which echo) "$line" | $(which mawk) '{print $1}'
+		app_index=$($(which echo) "$line" | $(which mawk) '{print $1}')
+		$(which pactl) move-sink-input "$app_index" "$(pactl get-default-sink)"
 	done
 
-	echo ""
-	echo "Normal audio settings restored successfully!"
+	# Determine that the standard audio options have been successfully restored
+	$(which echo) ""
+	$(which echo) "Normal audio settings restored successfully!"
 }
 
-# Function to activate Stream mode
+# Function to activate the Stream mode
 enable_stream()
 {
-	echo "Enabling audio settings for Stream..."
-	echo "Number of parameters : $#"
-	echo "List of Parameters   : $*"
+	$(which echo) "Enabling audio settings for Stream..."
+	$(which echo) "Number of parameters : $#"
+	$(which echo) "List of Parameters   : $*"
 	view_available_devices	# View available installed audio devices
 
 	# Assigning the new virtual devices to their respective source (input) and sink (output) files
-	echo ""
-	echo "Generating new virtual source-combining devices..."
+	$(which echo) ""
+	$(which echo) "Generating new virtual source-combining devices..."
 
 	# Create the virtual channel like '$1' and its description like '$2'
-	pactl load-module module-null-sink sink_name="$1" sink_properties="device.description='$2'" ||
-	{ echo "Error loading null-sink."; exit 1; }
+	$(which pactl) load-module module-null-sink sink_name="$1" sink_properties="device.description='$2'" ||
+	{ $(which echo) "Error loading null-sink."; exit 1; }
 
 	# Connect the channel source to the recording stream output (source, input)
 	# Output loopback (speakers)
-	pactl load-module module-loopback source="$(pactl get-default-source)" sink="$1" ||
-	{ echo "Error loading loopback for source (input)."; disable_stream; }
+	$(which pactl) load-module module-loopback source="$(pactl get-default-source)" sink="$1" ||
+	{ $(which echo) "Error loading loopback for source (input)."; disable_stream; }
 
 	# Connect the channel sink to the playback stream input (sink, output)
 	# Input loopback (microphone)
-	pactl load-module module-loopback source="$(pactl get-default-sink)" sink="$1" ||
-	{ echo "Error loading loopback for sink (output)."; disable_stream; }
+	$(which pactl) load-module module-loopback source="$(pactl get-default-sink)" sink="$1" ||
+	{ $(which echo) "Error loading loopback for sink (output)."; disable_stream; }
 
 	# Remapping the output sink to the specified destination source
-	pactl load-module module-remap-sink source="$1.monitor" sink_name="$1_$3" master="$(pactl get-default-sink)" sink_properties="device.description='$1_$2_$3'"
+	$(which pactl) load-module module-remap-sink source="$1.monitor" sink_name="$1_$3" master="$(pactl get-default-sink)" sink_properties="device.description='$1_$2_$3'"
 
 	# Set the sink (output) as default
-	pactl set-default-sink "$1_$3"
+	$(which pactl) set-default-sink "$1_$3"
 
 	# Redirect all applications to the combined sink
-	echo ""
-	echo "Redirecting all applications to the combined sink... ($1_$3)..."
+	$(which echo) ""
+	$(which echo) "Redirecting all applications to the combined sink... ($1_$3)..."
 
 	# Get the names of all audio applications and redirect them to the combined sink
-	pactl list short sink-inputs |
+	$(which pactl) list short sink-inputs |
 	while read -r line; do
 		# We obtain the application index
-		echo "$line" | mawk '{print $1}'
-		app_index=$(echo "$line" | mawk '{print $1}')
-		pactl move-sink-input "$app_index" "$1_$3"
+		$(which echo) "$line" | $(which mawk) '{print $1}'
+		app_index=$($(which echo) "$line" | $(which mawk) '{print $1}')
+		$(which pactl) move-sink-input "$app_index" "$1_$3"
 	done
 
-	echo ""
-	echo "Done!"
-	echo "Don't forget to configure '$1_$3' to output the music to '$2'."
+	# Full establishment of the new audio transmission channel
+	$(which echo) ""
+	$(which echo) "Done!"
+	$(which echo) "Don't forget to configure '$1_$3' to output the music to '$2'."
 }
 
 # Function to list the devices available on the system
 list_of_devices()
 {
-	echo "Linked sound devices."
-	echo "+ Output devices (sinks)."
-	pactl list short sinks
+	$(which echo) "Linked sound devices."
+	$(which echo) "+ Output devices (sinks)."
+	$(which pactl) list short sinks
 
-	echo ""
-	echo "+ Input devices (sources)."
-	pactl list short sources
+	$(which echo) ""
+	$(which echo) "+ Input devices (sources)."
+	$(which pactl) list short sources
 }
 
+# Function to briefly display the devices enabled in the system
 view_available_devices()
 {
-	echo "Available Devices."
-	echo "+ Source Input : $(pactl get-default-source)"	# Sources are input
-	echo "+ Sink Output  : $(pactl get-default-sink)"	# Sinks are output
+	$(which echo) "Available Devices."
+	$(which echo) "+ Source Input : $(pactl get-default-source)"	# Sources are input
+	$(which echo) "+ Sink Output  : $(pactl get-default-sink)"	# Sinks are output
 }
 
 # Simple menu
 counter=0
 option=0
 my_date=$(date "+%A, %B %d, %Y - %H:%M:%S")
+
+# Check variables with the path of each key command
+my_date_directory=$(which date)
+my_echo_directory=$(which echo)
 my_pactl_directory=$(which pactl)
 
 # Decision control if PulseAudio is installed.
-if [ -f $my_pactl_directory ]
+if [[ -f $my_pactl_directory && -f $my_date_directory && -f $my_echo_directory ]]
 then
-	echo "$my_date"
-	echo "Control a running PulseAudio sound server."
+	$(which echo) $my_date
+	$(which echo) "Control a running PulseAudio sound server."
+	$(which echo) $my_date_directory $my_echo_directory $my_pactl_directory
 
 	while [ $option -ne 5 ]
 	do
-		echo ""
-		date
-		echo "+===+====+===+===+====+===+====+===+====+"
-		echo "|Audio Mix Control for Ubuntu GNU/Linux.|"
-		echo "+===+====+===+===+====+===+====+===+====+"
-		echo "| [1]. Activate Stream mode.            |"
-		echo "| [2]. Deactivate Stream and clean mode.|"
-		echo "| [3]. List enable devices.             |"
-		echo "| [4]. View available devices.          |"
-		echo "| [5]. Leave this program.              |"
-		echo "+===+====+===+===+====+===+====+===+====+"
-		read -p "Select one option (1, 2, 3, 4 or 5): " option
+		$(which echo) ""
+		$(which date)
+		$(which echo) "+===+====+===+===+====+===+====+===+====+"
+		$(which echo) "|Audio Mix Control for Ubuntu GNU/Linux.|"
+		$(which echo) "+===+====+===+===+====+===+====+===+====+"
+		$(which echo) "| [1]. Activate Stream mode.            |"
+		$(which echo) "| [2]. Deactivate Stream and clean mode.|"
+		$(which echo) "| [3]. List enable devices.             |"
+		$(which echo) "| [4]. View available devices.          |"
+		$(which echo) "| [5]. Leave this program.              |"
+		$(which echo) "+===+====+===+===+====+===+====+===+====+"
+		read -p "Select one option (1-5): " option
 
 		counter=$(( $counter + 1 ))
-		echo ""
-		echo "Selected option: [$option]."
-		echo "Attempts made:   [$counter]."
-		echo ""
+		$(which echo) ""
+		$(which date) "+%Y-%m-%d - %H:%M:%S"
+		$(which echo) "Selected option: [$option]."
+		$(which echo) "Attempts made:   [$counter]."
+		$(which echo) "Key Operation:   [$RANDOM]."
+		$(which echo) ""
 
 		if [ $option -eq 1 ]; then
 			enable_stream "Stream_Mix" "Mix_For_Stream" "My_Output"
@@ -145,14 +155,16 @@ then
 		elif [ $option -eq 4 ]; then
 			view_available_devices
 		elif [ $option -eq 5 ]; then
-			echo "Leaving this program..."
+			$(which echo) "Leaving this program..."
 			break
 		else
-			echo "Invalid option: [$option]. Please correct it!"
+			$(which echo) "Invalid option: [$option]. Please correct it!"
 		fi
 	done
 else
-	echo "PulseAudio sound server does not activate!"
-	echo $my_pactl_directory
+	$(which echo) "PulseAudio sound server does not activate!"
+	$(which echo) $my_date_directory
+	$(which echo) $my_echo_directory
+	$(which echo) $my_pactl_directory
 	exit 1
 fi
